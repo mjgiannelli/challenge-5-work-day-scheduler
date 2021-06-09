19ec6561259f0c48614b7d576c607885480e5897
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-06-09 11:36:05 -0500

    add pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// add current day to display at top of page under header
+//  - will add text content of current day and append to the <p> element in the header in the format 'Name of Day', 'Full name of month', 'Numerical Day with suffix th or st'
+// add time blocks to div container using jquery (3 columns in a row, 24 rows):
+//  - first time in format '12AM'
+//  - then task container
+//  - then save icon
+// create formatting feature to color code time block:
+//  - grey for past
+//  - red for in progress
+//  - green for upcoming
+// add functionality so when user clicks into time block:
+//  - can edit the text content on focus
+//  - hardcode the content on blur
+// when user clicks the save button icon the text for the event saves into local storage via an array object
+// when user refreshes page - get the object array from local storage and recreate the events on the page
+
